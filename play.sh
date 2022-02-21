3e22f30bc33b3dc68f39cb5a35fbd8d833f6ac05
POWER <clay.degruchy+github@gmail.com>
2022-02-21 18:16:38 +0100

    not sure what went wrong here

--- play.sh
+++ play.sh
@@ -6,10 +6,11 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-pkill  "sox"
+pkill  "sox"  
 pkill  "play"
 
 librespot  --system-cache ./cache/ --name "Spotify Radio" --backend pipe --device /opt/music/spotify --bitrate 160 --disable-audio-cache --enable-volume-normalisation  &
+echo "If its not working, just kill and restart" & 
 sh radio_effect.sh $1 &
 wait 
 
